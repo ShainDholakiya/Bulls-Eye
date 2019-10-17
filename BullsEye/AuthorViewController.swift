1995d41f68e8cac2fec265696db91e48b570f856
Shain Dholakiya <shainshain99@gmail.com>
2019-10-16 20:47:09 -0400

    updates

---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Shain Dholakia on 10/14/19.
+//  Copyright © 2019 Shain Dholakiya. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
